bb2f5f80d58c8f04412e0ea53fc0f24bcdb0f670
Stefaan Lippens <soxofaan@gmail.com>
2011-12-22 02:51:54 +0100

    added README.rst

--- update.py
+++ update.py
@@ -8,8 +8,8 @@ Git update hook Nitty Committy
 Git update hook that manages a commit message histogram
 and complains about or blocks pushes with messages that are too common.
 
-Usage:
------
+Usage as hook
+-------------
 Copy/symlink update.py to hooks/update in the git repo to protect.
 
 This script will be invoked by git with three arguments: 
@@ -17,6 +17,8 @@ This script will be invoked by git with three arguments:
 2) current SHA1 of that ref (or 0000...0 for new refs)
 3) newly pushed SHA1 for the ref
 
+Additional usage
+----------------
 Additionally you can also invoke this script manually without with extra
 options (and possibly without those hook-related argments) 
 for inspection or additional functionality. 
